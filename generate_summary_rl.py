61ade0efe9175230c53050d080f74a8297728534
yg211 <gaoyang_alex@hotmail.com>
2020-12-07 19:00:23 +0000

    fix bug to allow rl summarizer to change summary length

--- generate_summary_rl.py
+++ generate_summary_rl.py
@@ -30,7 +30,7 @@ class RLSummarizer():
     def summarize(self, docs, summ_max_len=100):
         # generate sample summaries for memory replay
         summaries, rewards = self.get_sample_summaries(docs, summ_max_len)
-        vec = Vectoriser(docs, sum_len=summ_max_len, base=self.base_length,)
+        vec = Vectoriser(docs, sum_len=summ_max_len, base=self.base_length)
         rl_agent = RLAgent(vec, summaries, strict_para=self.rl_strict, train_round=self.train_episode, gpu=self.gpu)
         summary = rl_agent(rewards)
         return summary
